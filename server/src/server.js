f6ceb3b7409489866237450dceebae317fc87573
yahya benzha <yahya_benzha@um5.ac.ma>
2022-01-29 06:23:47 +0100

    fullstack automation

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,12 @@
 const http = require("http");
-
+const cors = require("cors");
 const app = require("./app");
 const PORT = process.env.PORT || 8000;
-
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
