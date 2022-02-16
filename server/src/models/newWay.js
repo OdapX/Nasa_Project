9157c3ef34bfc2ab0bf525620cc1e9b9a699d3e8
yahya benzha <yahya_benzha@um5.ac.ma>
2022-02-16 19:25:03 +0100

    fixing path issues

--- server/src/models/newWay.js
+++ server/src/models/newWay.js
@@ -1,36 +1,18 @@
-// const mongojs = require("mongojs");
-
-// const DB_URL =
-//   "mongodb+srv://damn:nVp14K0v2957f6we@db-mongodb-nyc3-22247-7d522c30.mongo.ondigitalocean.com/education?authSource=admin&replicaSet=db-mongodb-nyc3-22247&tls=true&tlsCAFile=./database.crt";
-
-// const collections = ["word"];
-
-// const db = mongojs(DB_URL, collections);
-
-// db.on("error", function (error) {
-//   console.log("Database Error:", error);
-// });
-// db.word.find({}, function (error, found) {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log(found);
-//     db.close();
-//     return;
-//   }
-// });
-
 const { MongoClient } = require("mongodb");
 // or as an es module:
 // import { MongoClient } from 'mongodb'
 
+const path = require("path");
 // Connection URL
-const url =
-  "mongodb+srv://odap:benzover145145@nasacluster.mpow6.mongodb.net/NasaCluster?retryWrites=true&w=majority";
+console.log(path.join(__dirname, "database.crt"));
+const url = `mongodb+srv://damn:nVp14K0v2957f6we@db-mongodb-nyc3-22247-7d522c30.mongo.ondigitalocean.com/healthcare?authSource=admin&replicaSet=db-mongodb-nyc3-22247&tls=true&tlsCAFile=${path.join(
+  __dirname,
+  "database.crt"
+)}`;
 const client = new MongoClient(url);
 
 // Database Name
-const dbName = "NasaCluster";
+const dbName = "healthcare";
 
 async function main() {
   // Use connect method to connect to the server
@@ -39,7 +21,7 @@ async function main() {
   });
   console.log("Connected successfully to server");
   const db = client.db(dbName);
-  const collection = db.collection("launches");
+  const collection = db.collection("cancer");
   console.log(await collection.find({}).toArray());
   client.close();
   // the following code examples can be pasted here...
