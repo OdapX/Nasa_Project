795d5f6f10df5df05af81e62930fd04f8cf18a5a
yahya benzha <yahya_benzha@um5.ac.ma>
2022-02-17 06:05:02 +0100

    capturing some tests

--- server/src/models/newWay.js
+++ server/src/models/newWay.js
@@ -1,38 +1,53 @@
-const { MongoClient } = require("mongodb");
-// or as an es module:
-// import { MongoClient } from 'mongodb'
+// const { MongoClient } = require("mongodb");
+// // or as an es module:
+// // import { MongoClient } from 'mongodb'
 
-const path = require("path");
-// Connection URL
-console.log(path.join(__dirname, "database.crt"));
-const url = `mongodb+srv://damn:nVp14K0v2957f6we@db-mongodb-nyc3-22247-7d522c30.mongo.ondigitalocean.com/healthcare?authSource=admin&replicaSet=db-mongodb-nyc3-22247&tls=true&tlsCAFile=${path.join(
-  __dirname,
-  "database.crt"
-)}`;
-const client = new MongoClient(url);
+// const path = require("path");
 
-// Database Name
-const dbName = "healthcare";
+// const url =
+//   "mongodb+srv://odap:benzover145145@nasacluster.mpow6.mongodb.net/NasaCluster?retryWrites=true&w=majority";
+// const client = new MongoClient(url);
 
-async function main() {
-  // Use connect method to connect to the server
-  await client.connect().catch((Err) => {
-    console.log("Error connecting to");
-  });
-  console.log("Connected successfully to server");
-  const db = client.db(dbName);
-  const collection = db.collection("cancer");
-  console.log(await collection.find({}).toArray());
-  client.close();
-  // the following code examples can be pasted here...
-}
+// // Database Name
+// const dbName = "NasaCluster";
 
-main()
-  .then((Res) => {
-    console.log(Res);
-    return;
-  })
-  .catch((err) => {
-    console.log(err);
-    return;
-  });
+// async function main() {
+//   // Use connect method to connect to the server
+//   await client.connect().catch((Err) => {
+//     console.log("Error connecting to");
+//   });
+//   console.log("Connected successfully to server");
+//   const db = client.db(dbName);
+//   const collection = db.collection("launches");
+//   const keys = await collection.findOne(
+//     {},
+//     {
+//       _id: 0,
+//       __v: 0,
+//     }
+//   );
+//   for (key in keys) {
+//     if (key != "_id") console.log(key);
+//   }
+
+//   client.close();
+//   // the following code examples can be pasted here...
+// }
+
+// main()
+//   .then((Res) => {
+//     console.log(Res);
+//     return;
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//     return;
+//   });
+
+const s = "addffdff $dfdfdf dfdff";
+const L = s.split(" ");
+L.forEach((e) => {
+  if (e.startsWith("$")) {
+    console.log(e);
+  }
+});
