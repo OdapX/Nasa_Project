ec8fc2fafa3d0a5df512ce8141a4b0d993a9525b
yahya benzha <yahya_benzha@um5.ac.ma>
2022-02-23 03:13:54 +0100

    issues with mongo performance

--- server/src/models/newWay.js
+++ server/src/models/newWay.js
@@ -1,87 +1,38 @@
-// const { MongoClient } = require("mongodb");
+const { MongoClient } = require("mongodb");
 
-// const path = require("path");
+const path = require("path");
 
-// const url =
-//   "mongodb+srv://odap:benzover145145@nasacluster.mpow6.mongodb.net/NasaCluster?retryWrites=true&w=majority";
-// const client = new MongoClient(url);
+const url = `mongodb+srv://damn:nVp14K0v2957f6we@db-mongodb-nyc3-22247-7d522c30.mongo.ondigitalocean.com/location?authSource=admin&replicaSet=db-mongodb-nyc3-22247&tls=true&tlsCAFile=${path.join(
+  __dirname,
+  "database.crt"
+)}`;
 
-// // Database Name
-// const dbName = "NasaCluster";
+const client = new MongoClient(url);
 
-// async function main() {
-//   // Use connect method to connect to the server
-//   await client.connect().catch((Err) => {
-//     console.log("Error connecting to");
-//   });
-//   console.log("Connected successfully to server");
-//   const db = client.db(dbName);
-//   const collection = db.collection("launches");
-//   const data = await collection
-//     .find(
-//       {},
-//       {
-//         _id: 0,
-//         __v: 0,
-//       }
-//     )
-//     .toArray();
-//   client.close();
-//   return data;
+// Database Name
+const dbName = "location";
 
-//   // the following code examples can be pasted here...
-// }
-// const combineData = (input, command, data) => {
-//   command = "$" + command;
-//   const L = input.split(" ");
-//   L[L.indexOf(command)] = data;
-//   let final_value = "";
-//   L.forEach((e) => {
-//     final_value = final_value + e + " ";
-//   });
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect().catch((Err) => {
+    console.log(Err);
+  });
 
-//   return final_value;
-// };
-// const input = "give me in Rocket s";
-// const getCommand = (input) => {
-//   const L = input.split(" ");
-//   let command = "";
-//   L.forEach((e) => {
-//     if (e.startsWith("$")) {
-//       command = e.substring(1);
-//     }
-//   });
-//   return command;
-// };
-// console.log(getCommand(input) == "");
-// // main()
-// //   .then((Res) => {
-// //     Res.map((element) => {
-// //       console.log(
-// //         combineData(input, getCommand(input), element[getCommand(input)])
-// //       );
-// //     });
-// //     return;
-// //   })
-// //   .catch((err) => {
-// //     console.log(err);
-// //     return;
-// //   });
+  const db = client.db(dbName);
+  const L = [];
+  const collection = db.collection("region");
+  await collection
+    .find(
+      {},
+      {
+        _id: 0,
+      }
+    )
+    .limit(20000)
+    .toArray();
+  client.close();
 
-// // const s = "aew $command xcc";
+  return L;
+}
 
-// // console.log(getCommand(s));
-
-// // const L = [
-// //   { a: 1, b: 2 },
-// //   { a: 3, b: 4 },
-// // ];
-// // const xxx = "b";
-// // L.map((e) => {
-// //   console.log(e[xxx]);
-// // });
-
-// //get(e from subcategory in database)
-
-require("dotenv").config();
-console.log(typeof process.env.DB_URL);
+main().then((data) => console.log(data));
